7809d4d6bd0589ae3b8f4879af6ca568ad841291
Timo Furrer <tuxtimo@gmail.com>
2013-09-20 17:35:08 +0200

    setup: add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pymailed",
+    description="pythonic driver for mail led",
+    version="0.00.01",
+    author="Timo Furrer",
+    author_email="tuxtimo@gmail.com",
+    url="https://github.com/timofurrer/pymailed",
+    packages=["pymailed"]
+)
